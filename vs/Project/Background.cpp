06ea408901be2b3503171f95a3d7cbd012a6ed2a
andinaufal0608@gmail.com <andinaufal0608@gmail.com>
2022-07-07 10:20:22 +0700

    background

---
+++ vs/Project/Background.cpp
@@ -0,0 +1,10 @@
+#include "Background.h"
+
+Engine::Background::~Background()
+{
+}
+
+void Engine::Background::UpdateObject(int score, float gameTime, Setting* setting)
+{
+
+}
---
+++ vs/Project/Background.h
@@ -0,0 +1,20 @@
+#ifndef Backgound_H
+#define Background_H
+
+#include "Sprite.h"
+#include "Setting.h"
+#include "Texture.h"
+#include "Sprite.h"
+namespace Engine {
+	class Background :
+		public Engine::Sprite
+	{
+    public:
+        Background(Texture* texture, Shader* shader, Quad* quad, Setting* setting) :Sprite(texture, shader, quad) {
+            this->SetPosition(0,0);
+        };
+        ~Background();
+        void UpdateObject(int score, float gameTime, Setting* setting);
+    };
+}
+#endif
